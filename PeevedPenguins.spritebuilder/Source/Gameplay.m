6f3f9608e68e3e0bea5fb2e10579689ff45cf918
Jacob Haff <jhaff@Jacobs-MacBook-Pro.local>
2014-06-27 10:16:21 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -46,6 +46,11 @@
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
     [self runAction:follow];
+    
+    - (void)retry {
+        // reload this level
+        [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+    }
 
 }
 
