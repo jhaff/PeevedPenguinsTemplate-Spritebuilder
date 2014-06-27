8904616a3af5f9f3c8e9307950bf9d4dfd69aae7
Jacob Haff <jhaff@Jacobs-MacBook-Pro.local>
2014-06-27 04:58:50 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,13 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Jacob Haff on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Jacob Haff on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
