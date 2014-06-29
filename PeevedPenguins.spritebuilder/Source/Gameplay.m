dc3a8dd345b589c4ef474737b7125fc14b46d7c4
Jacob Haff <jhaff@jacobs-mbp.home>
2014-06-29 12:14:14 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -156,7 +156,7 @@
     
     CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
     // make the particle effect clean itself up, once it is completed
-    explosion.autoRemoveOnFinish = NO;
+    explosion.autoRemoveOnFinish = YES;
     // place the particle effect on the seals position
     explosion.position = seal.position;
     // add the particle effect to the same node the seal is on
